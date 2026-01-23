b47fb5f700cd6037568cb9ca852467197f319398
Chameleon Cloud User <cc@spark.novalocal>
2026-01-22 21:09:33 -0500

    complete procinfo

--- src/procinfo.c
+++ src/procinfo.c
@@ -16,6 +16,9 @@ int main(int c,char**v){
   snprintf(path, sizeof(path), "/proc/%d/cmdline", pid);
   FILE *f2 = fopen(path, "r");
 
+  snprintf(path, sizeof(path), "/proc/%d/status", pid);
+  FILE *f3 = fopen(path, "r");
+
   char buf[4096];
   if (fgets(buf, sizeof(buf), f1) == NULL) {
     perror("fgets");
@@ -38,9 +41,33 @@ int main(int c,char**v){
     token = strtok(NULL, " \t\n");
   }
 
+  char *utime_str = tokens[15];
+  char *stime_str = tokens[16];
+
+  unsigned long long utime = strtoull(utime_str, NULL, 10);
+  unsigned long long stime = strtoull(stime_str, NULL, 10);
+
+  unsigned long long total_ticks = utime + stime;
+  long clk = sysconf(_SC_CLK_TCK);
+  double cpu_sec = (double)total_ticks / (double)clk;
+
+  char line[256];
+  long rss_kb = -1;
+  while (fgets(line, sizeof(line), f3)) {
+	  if (strncmp(line, "VmRSS:", 6) == 0) {
+		  sscanf(line + 6, "%ld", &rss_kb);
+		  break;
+	  }
+  }
+  fclose(f3);
+
+  char cmdline[256];
+
   printf("PID:%s\n", tokens[0]);
   printf("State:%s\n", tokens[2]);
   printf("PPID:%s\n", tokens[3]);
-  printf("Cmd:%s\n", fgets(buf, sizeof(buf), f2));
+  printf("Cmd:-%s\n", fgets(cmdline, sizeof(cmdline), f2));
+  printf("CPU: %llu %.3f\n", total_ticks, cpu_sec);
+  printf("VmRSS: %ld\n", rss_kb);
   return 0;
 }
